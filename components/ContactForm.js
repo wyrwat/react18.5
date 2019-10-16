27ff8fb188b08909ead4b24fda0ebd8d1bb76b20
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-10-16 11:39:50 +0200

    Add label to ConactForm

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -6,20 +6,35 @@ var ContactForm = React.createClass({
     render: function() {
       return (
         React.createElement('form', {className: 'contactForm'},
+          React.createElement('label', {
+            for: 'nameInput',
+          }),
           React.createElement('input', {
+            id: 'nameInput',
             type: 'text',
             placeholder: 'Name',
             value: this.props.contact.firstName,
+            name: 'name',
+          }),
+          React.createElement('label', {
+            for: 'surnameInput',
           }),
           React.createElement('input', {
+            id: 'surnameInput',
             type: 'text',
             placeholder: 'Surname',
             value: this.props.contact.lastName,
+            name: 'surname',
+          }),
+          React.createElement('label', {
+            for: 'emailInput',
           }),
           React.createElement('input', {
+            id: 'emailInput',
             type: 'email',
             placeholder: 'Email',
             value: this.props.contact.email,
+            name: 'email',
           }),
           React.createElement('button', {type: 'submit'}, "Add contact")
         )
